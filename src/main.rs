609d8c289cab7369f9e261c36bda73b99f32db23
nanato12 <admin@nanato12.info>
2021-09-23 01:01:52 +0900

    update: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // mapやfilter
+    let source = vec![1, 2, 3, 4, 5];
+    let result = source
+        .into_iter()
+        .filter(|n| n % 2 == 0)
+        .map(|n| n.to_string())
+        .collect::<Vec<String>>();
+    println!("{:?}", result);
+
+    // Optional
+    let objective = Some(1);
+    match objective {
+        Some(x) if x % 2 == 0 => println!("偶数: {}", x),
+        Some(x) => println!("奇数: {}", x),
+        None => println!("値なし"),
+    }
+
+    // 型推論
+    let mut v = vec![];
+    v.push(1);
+    v.push(64);
+    println!("{:?}", v);
 }
